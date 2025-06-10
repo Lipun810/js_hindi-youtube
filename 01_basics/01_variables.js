8f108b95c964e371e6d3b58f779b45781d55a2e6
Lipun810 <lipunghosh@gmail.com>
2025-06-10 13:35:02 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "lipunghosh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jamsedhpur"
+
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bangalore"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
