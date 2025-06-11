9a641d844ccb633865b937513a080f610e26215f
Lipun810 <lipunghosh@gmail.com>
2025-06-11 09:39:01 +0530

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,31 @@ const repocount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
 
-const gameName = new String('Lipun')
+const gameName = new String('Lipun-lg')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('p'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Lipun   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hites.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('Sunder'));
+
+console.log(gameName.split('-'));
