c33b563e2c690af9e7cf9d55dc98994dbf77cff7
Lipun810 <lipunghosh@gmail.com>
2025-06-11 08:55:24 +0530

    first commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Lipun"
+const repocount = 50
+
+// console.log(name +repocount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('Lipun')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
